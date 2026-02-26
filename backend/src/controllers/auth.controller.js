cfc6734bf3c4f1a51465e54a5060d72e1b2bb805
robert <robert.walker6580@gmail.com>
2026-02-26 12:43:03 -0800

    Implement create user

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,67 @@
+import User from '../models/User.js';
+import jwt from 'jsonwebtoken';
+
 export const signup = async (req, res) => {
-  res.send('Signup route');
+  const { fullName, email, password } = req.body;
+
+  try {
+    if (!fullName || !email || !password) {
+      return res.status(400).json({ message: 'All fields are required' });
+    }
+
+    if (password.length < 8) {
+      return res
+        .status(400)
+        .json({ message: 'Password must be at least 8 characters long' });
+    }
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({ message: 'Invalid email format' });
+    }
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: 'Email already in use' });
+    }
+
+    const idx = Math.floor(Math.random() * 100) + 1; // generate a random number between 1-100
+    const randomAvatar = `https://avatar.iran.liara.run/public/${idx}.png`;
+
+    const newUser = await User.create({
+      fullName,
+      email,
+      password,
+      profilePic: randomAvatar,
+    });
+
+    // TODO: CREATE THE USER IN STREAM AS WELL
+
+    const token = jwt.sign(
+      { userId: newUser._id },
+      process.env.JWT_SECRET_KEY,
+      {
+        expiresIn: '7d',
+      }
+    );
+
+    res.cookie('jwt', token, {
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+      httpOnly: true, // prevent client-side JS from accessing the cookie
+      sameSite: 'strict', // CSRF protection
+      secure: process.env.NODE_ENV === 'production', // use secure cookies in production
+    });
+
+    res.status(201).json({
+      success: true,
+      message: 'User created successfully',
+      user: newUser,
+    });
+  } catch (error) {
+    console.log('Error in signup controller: ', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
 };
 
 export const login = async (req, res) => {
