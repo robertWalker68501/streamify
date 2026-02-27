39a6b0e2b61ecbc950f13b97f1d82d57652689af
robert <robert.walker6580@gmail.com>
2026-02-27 14:34:15 -0800

    Implement onboarding

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import 'dotenv/config';
+import cookieParser from 'cookie-parser';
+
 import authRoutes from './routes/auth.route.js';
 import { connectDB } from './lib/db.js';
 
@@ -7,6 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use('/api/auth', authRoutes);
 
